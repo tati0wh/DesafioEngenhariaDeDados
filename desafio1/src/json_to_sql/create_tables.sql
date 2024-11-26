41cba50f5eed1a89a34ae58d47e9fcfa9210e2bf
Tatiana <126476937+tati0wh@users.noreply.github.com>
2024-11-26 15:00:11 -0300

    Add files via upload

--- desafio1/src/json_to_sql/create_tables.sql
+++ desafio1/src/json_to_sql/create_tables.sql
@@ -1,1 +1,40 @@
+-- SQL Script para tabela GuestChecks
+CREATE TABLE "GuestChecks" (
+"guestCheckId" INTEGER,
+  "chkNum" INTEGER,
+  "opnBusDt" TEXT,
+  "clsdBusDt" TEXT,
+  "subTtl" REAL,
+  "chkTtl" REAL,
+  "dscTtl" INTEGER,
+  "empNum" INTEGER
+);
+
+-- SQL Script para tabela DetailLines
+CREATE TABLE "DetailLines" (
+"guestCheckLineItemId" INTEGER,
+  "guestCheckId" INTEGER,
+  "lineNum" INTEGER,
+  "dspTtl" REAL,
+  "chkEmpId" INTEGER,
+  "svcRndNum" INTEGER
+);
+
+-- SQL Script para tabela MenuItems
+CREATE TABLE "MenuItems" (
+"guestCheckLineItemId" INTEGER,
+  "miNum" INTEGER,
+  "modFlag" INTEGER,
+  "inclTax" REAL,
+  "prcLvl" INTEGER
+);
+
+-- SQL Script para tabela Taxes
+CREATE TABLE "Taxes" (
+"guestCheckId" INTEGER,
+  "taxNum" INTEGER,
+  "txblSlsTtl" REAL,
+  "taxCollTtl" REAL,
+  "taxRate" INTEGER
+);
 
